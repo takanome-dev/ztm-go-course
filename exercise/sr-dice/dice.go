966f4f2cc7e08ebfc2f4441cce927718a8936c1b
takanome_dev <takanomedev221@gmail.com>
2023-09-01 21:05:17 +0000

    simple dice roll game giving me headaches :)

--- exercise/sr-dice/dice.go
+++ exercise/sr-dice/dice.go
@@ -4,7 +4,7 @@
 //
 //--Requirements:
 //* Print the sum of the dice roll
-//* Print additional information in these cirsumstances:
+//* Print additional information in these circumstances:
 //  - "Snake eyes": when the total roll is 2, and total dice is 2
 //  - "Lucky 7": when the total roll is 7
 //  - "Even": when the total roll is even
@@ -16,7 +16,42 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func roll(sides int) int {
+	return rand.Intn(sides) + 1
+}
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+	
+	dice, sides := 2, 6
+	rolls := 2
+
+	for r := 1; r <= rolls; r++ {
+		sum := 0
+
+		for d := 1; d <= dice; d++ {
+			rolled := roll(sides)
+			sum += rolled
+			fmt.Println("Roll #", r, ", Die #", d, ":", rolled)
+		}
+
+		fmt.Println("Total rolled:", sum)
+
+		switch sum := sum; {
+			case sum == 2 && dice == 2:
+				fmt.Println("Snake eyes")
+			case sum == 7:
+				fmt.Println("Lucky 7")
+			case sum % 2 == 0:
+				fmt.Println("Even")
+			case sum % 2 == 1:
+				fmt.Println("Odd")
+		}
+	}
 }
