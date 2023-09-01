8e9345a178786ce50764c0761242036e709c21c6
takanome_dev <takanomedev221@gmail.com>
2023-09-01 19:11:49 +0000

    playing around with loops

--- demo/loops/loops.go
+++ demo/loops/loops.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
+	sum := 0
+	fmt.Println("initial sum is", sum)
 
+	for i := 1; i <= 10; i++ {
+		sum += i
+		fmt.Println("new sum is", sum)
+	}
+
+	for sum > 10 {
+		sum -= 5
+		fmt.Println("Decremented - sum is", sum)
+	}
 }
