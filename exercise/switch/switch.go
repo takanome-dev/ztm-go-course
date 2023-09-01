1cfc47179e75f6a7d0d7e53fe04c8c242249f113
takanome_dev <takanomedev221@gmail.com>
2023-09-01 18:09:28 +0000

    done exo on switch statements

--- exercise/switch/switch.go
+++ exercise/switch/switch.go
@@ -14,4 +14,16 @@ package main
 import "fmt"
 
 func main() {
+	switch age := 1000; {
+		case age == 0:
+			fmt.Println("newborn")
+		case age >= 1 && age <= 3:
+			fmt.Println("toddler")
+		case age >= 4 && age <= 12:
+			fmt.Println("child")
+		case age >= 13 && age <= 17:
+			fmt.Println("teenager")
+		default:
+			fmt.Println("adult")
+	}
 }
