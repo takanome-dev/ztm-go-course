2fd7cd2d9032613415a9afc6790f6850498aec51
takanome_dev <takanomedev221@gmail.com>
2023-09-01 23:23:27 +0000

    structs are very cool, but struggles with the math exo :)

--- exercise/structs/structs.go
+++ exercise/structs/structs.go
@@ -3,13 +3,6 @@
 //  of a rectangle.
 //
 //--Requirements:
-//* Create a rectangle structure containing its coordinates
-//* Using functions, calculate the area and perimeter of a rectangle,
-//  - Print the results to the terminal
-//  - The functions must use the rectangle structure as the function parameter
-//* After performing the above requirements, double the size
-//  of the existing rectangle and repeat the calculations
-//  - Print the new results to the terminal
 //
 //--Notes:
 //* The area of a rectangle is length*width
@@ -19,6 +12,49 @@ package main
 
 import "fmt"
 
-func main() {
+type Coordinate struct {
+	x, y int
+}
 
+// * Create a rectangle structure containing its coordinates
+type Rectangle struct {
+	a Coordinate // top left
+	b Coordinate // bottom right
+}
+
+// * Using functions, calculate the area and perimeter of a rectangle,
+//   - Print the results to the terminal
+//   - The functions must use the rectangle structure as the function parameter
+func width(rect Rectangle) int {
+	return (rect.b.x - rect.a.x)
+}
+
+func length(rect Rectangle) int {
+	return (rect.a.y - rect.b.y)
+}
+
+func area(rect Rectangle) int {
+	return length(rect) * width(rect)
+}
+
+func perimeter(rect Rectangle) int {
+	return (width(rect) * 2) + (length(rect) * 2)
+}
+
+func printInfo(rect Rectangle) {
+	fmt.Println("Area is", area(rect))
+	fmt.Println("Perimeter is", perimeter(rect))
+}
+
+func main() {
+	rect := Rectangle{a: Coordinate{0, 7}, b: Coordinate{10, 0}}
+	printInfo(rect)
+
+	//* After performing the above requirements, double the size
+	//  of the existing rectangle and repeat the calculations
+	//  - Print the new results to the terminal
+	rect.a.y *= 2
+	rect.b.x *= 2
+
+	printInfo(rect)
 }
