cf9ec2e8dc7ec8f32c47819e032bbfe9d9ba23b2
takanome_dev <takanomedev221@gmail.com>
2023-09-02 20:10:21 +0000

    ranges seems kinda cool 😏

--- demo/ranges/ranges.go
+++ demo/ranges/ranges.go
@@ -3,4 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	slice := []string{"Hello", "world", "!"}
+
+	for i, element := range slice {
+		fmt.Println("index:", i, "element:", element)
+
+		for _, char := range element {
+			fmt.Printf("  %q\n", char)
+		}
+	}
 }
