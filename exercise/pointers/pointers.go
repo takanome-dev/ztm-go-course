a457764af0eabfac23c933a4fd1af54a348229a0
takanome_dev <takanomedev221@gmail.com>
2023-09-03 16:27:39 +0000

    new thing for me, pointers 🔺

--- exercise/pointers/pointers.go
+++ exercise/pointers/pointers.go
@@ -3,21 +3,59 @@
 //  on products.
 //
 //--Requirements:
-//* Create a structure to store items and their security tag state
-//  - Security tags have two states: active (true) and inactive (false)
-//* Create functions to activate and deactivate security tags using pointers
-//* Create a checkout() function which can deactivate all tags in a slice
-//* Perform the following:
-//  - Create at least 4 items, all with active security tags
-//  - Store them in a slice or array
-//  - Deactivate any one security tag in the array/slice
-//  - Call the checkout() function to deactivate all tags
-//  - Print out the array/slice after each change
 
 package main
 
 import "fmt"
 
-func main() {
+const (
+	Active   = true
+	Inactive = false
+)
 
+// * Create a structure to store items and their security tag state
+//   - Security tags have two states: active (true) and inactive (false)
+type SecurityTag bool
+
+type Item struct {
+	name string
+	tag  SecurityTag
+}
+
+// * Create functions to activate and deactivate security tags using pointers
+func activate(tag *SecurityTag) {
+	*tag = Active
+}
+
+func deactivate(tag *SecurityTag) {
+	*tag = Inactive
+}
+
+// * Create a checkout() function which can deactivate all tags in a slice
+func checkout(items []Item) {
+	fmt.Println("Checking out...")
+
+	for _, item := range items {
+		deactivate(&item.tag)
+	}
+}
+
+func main() {
+	//* Perform the following:
+	//  - Create at least 4 items, all with active security tags
+	shirts := Item{"Shirt", Active}
+	pants := Item{"Pants", Active}
+	shoes := Item{"Shoes", Active}
+	purse := Item{"Purse", Active}
+
+	//  - Store them in a slice or array
+	items := []Item{shirts, pants, shoes, purse}
+	fmt.Println(items)
+
+	//  - Deactivate any one security tag in the array/slice
+	deactivate(&items[0].tag)
+	fmt.Println("Item 0 deactivated")
+
+	//  - Call the checkout() function to deactivate all tags
+	checkout(items)
 }
