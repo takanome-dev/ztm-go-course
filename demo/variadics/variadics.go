b636901e20b4e3d5cdca226b489f9e81ae9a562d
takanome_dev <takanomedev221@gmail.com>
2023-09-04 18:20:27 +0000

    so variadics is just the spread operator in js, kinda

--- demo/variadics/variadics.go
+++ demo/variadics/variadics.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
-func main() {
+func sum(nums ...int) int {
+	sum := 0
 
+	for _, num := range nums {
+		sum += num
+	}
+
+	return sum
+}
+
+func main() {
+	a := []int{2, 3, 5, 1}
+	b := []int{1, 2, 7, 4, 9}
+
+	sumOfA := sum(a...)
+	fmt.Println("The sum of a is:", sumOfA)
+
+	sumOfB := sum(b...)
+	fmt.Println("The sum of b is:", sumOfB)
 }
