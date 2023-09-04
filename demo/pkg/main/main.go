6924035e543a031dbde5568150d8fa959c1a7419
takanome_dev <takanomedev221@gmail.com>
2023-09-04 18:44:18 +0000

    wow creating pkgs is easier than i thought :)

---
+++ demo/pkg/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"coursecontent/demo/pkg/display"
+	"coursecontent/demo/pkg/msg"
+)
+
+func main() {
+	msg.Hi()
+	display.Display("hello from display")
+	msg.Exciting("an exciting message")
+}
