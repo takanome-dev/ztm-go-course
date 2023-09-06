b1ebd8d6d66fe446df7c406dfe35a19d2253412f
takanome_dev <takanomedev221@gmail.com>
2023-09-06 22:01:29 +0000

    thank God no try/catch for error handling 😂

--- demo/errors/errors.go
+++ demo/errors/errors.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
-func main() {
+type Stuff struct {
+	values []int
+}
 
+func (s *Stuff) Get(idx int) (int, error) {
+	if idx > len(s.values) {
+		return 0, errors.New(fmt.Sprintf("no element at index %v", idx))
+	}
+
+	return s.values[idx], nil
+}
+
+func main() {
+	stuff := Stuff{}
+
+	value, err := stuff.Get(1)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("value is", value)
+	}
 }
