341ae291182ff7fd0553162873d18bdbdccce8f5
takanome_dev <takanomedev221@gmail.com>
2023-09-07 08:27:11 +0000

    type embeddings are great ✨

--- exercise/embedding/embedding.go
+++ exercise/embedding/embedding.go
@@ -31,8 +31,54 @@ type MemoryUsage struct {
 	amount []Bytes
 }
 
+func (b *BandwidthUsage) AverageBandwidth() int {
+	sum := 0
+
+	for i := 0; i < len(b.amount); i++ {
+		sum += int(b.amount[i])
+	}
+
+	return sum / len(b.amount)
+}
+
+func (c *CpuTemp) AverageCpuTemp() int {
+	sum := 0
+
+	for i := 0; i < len(c.temp); i++ {
+		sum += int(c.temp[i])
+	}
+
+	return sum / len(c.temp)
+}
+
+func (m *MemoryUsage) AverageMemory() int {
+	sum := 0
+
+	for i := 0; i < len(m.amount); i++ {
+		sum += int(m.amount[i])
+	}
+
+	return sum / len(m.amount)
+}
+
+type Dashboard struct {
+	BandwidthUsage
+	CpuTemp
+	MemoryUsage
+}
+
 func main() {
 	bandwidth := BandwidthUsage{[]Bytes{50000, 100000, 130000, 80000, 90000}}
 	temp := CpuTemp{[]Celcius{50, 51, 53, 51, 52}}
 	memory := MemoryUsage{[]Bytes{800000, 800000, 810000, 820000, 800000}}
+
+	dash := Dashboard{
+		BandwidthUsage: bandwidth,
+		CpuTemp:        temp,
+		MemoryUsage:    memory,
+	}
+
+	fmt.Println("Average bandwidth:", dash.AverageBandwidth())
+	fmt.Println("Average Cpu Temp:", dash.AverageCpuTemp())
+	fmt.Println("Average memory:", dash.AverageMemory())
 }
