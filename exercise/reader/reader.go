dedf65c455b7c1cfab1ba1b4cdf93c8a9d8e8969
takanome_dev <takanomedev221@gmail.com>
2023-09-07 07:39:13 +0000

    messing around with readers & writers

--- exercise/reader/reader.go
+++ exercise/reader/reader.go
@@ -18,8 +18,44 @@
 
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+const (
+	CmdHello   = "hello"
+	CmdGoodBye = "bye"
+)
 
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	numLines := 0
+	numCommands := 0
 
+	for scanner.Scan() {
+		if strings.ToUpper(scanner.Text()) == "Q" {
+			break
+		} else {
+			text := strings.TrimSpace(scanner.Text())
+
+			switch text {
+			case CmdHello:
+				numCommands += 1
+				fmt.Println("command response: hi")
+			case CmdGoodBye:
+				numCommands += 1
+				fmt.Println("command response: bye")
+			}
+
+			if text != "" {
+				numLines += 1
+			}
+		}
+	}
+
+	fmt.Printf("You entered %v lines\n", numLines)
+	fmt.Printf("You entered %v commands\n", numCommands)
 }
