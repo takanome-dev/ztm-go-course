7dc512ffa358e6dcf13e331259b134520579d0d2
takanome_dev <takanomedev221@gmail.com>
2023-09-09 22:20:49 +0000

    function literals are easy to understand bcoz I used to write callback hell 😂

--- exercise/func-literal/func-literal.go
+++ exercise/func-literal/func-literal.go
@@ -18,7 +18,58 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode"
+)
+
+func numOfLetters(t string) int {
+	total := 0
+
+	for _, char := range t {
+		if unicode.IsLetter(char) {
+			total += 1
+		}
+	}
+
+	return total
+}
+
+func numOfDigits(t string) int {
+	total := 0
+
+	for _, char := range t {
+		if unicode.IsDigit(char) {
+			total += 1
+		}
+	}
+
+	return total
+}
+
+func numOfSpaces(t string) int {
+	total := 0
+
+	for _, char := range t {
+		if unicode.IsSpace(char) {
+			total += 1
+		}
+	}
+
+	return total
+}
+
+func numOfPunct(t string) int {
+	total := 0
+
+	for _, char := range t {
+		if unicode.IsPunct(char) {
+			total += 1
+		}
+	}
+
+	return total
+}
 
 func main() {
 	lines := []string{
@@ -28,4 +79,19 @@ func main() {
 		"12 spaces,",
 		"and 4 punctuation marks in these lines of text!",
 	}
+
+	totalLetters, totalDigits := 0, 0
+	totalSpaces, totalPunct := 0, 0
+
+	for _, text := range lines {
+		totalDigits += numOfDigits(text)
+		totalLetters += numOfLetters(text)
+		totalSpaces += numOfSpaces(text)
+		totalPunct += numOfPunct(text)
+	}
+
+	fmt.Printf("The total of letters is %v\n", totalLetters)
+	fmt.Printf("The total of digits is %v\n", totalDigits)
+	fmt.Printf("The total of spaces is %v\n", totalSpaces)
+	fmt.Printf("The total of punctuations is %v\n", totalPunct)
 }
